fffd83220eb2a10da0c3f6b8388467691d8245f2
Faris Rizvanovic <faris.rizvanovic00@gmail.com>
2022-10-13 23:40:20 +0200

    started contact page

---
+++ website/pages/contact.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Searchbar from "../components/Searchbar";
+import styles from "../styles/Home.module.css";
+
+export default function Contact() {
+  return (
+    <div>
+      <Head>
+        <title>Biblioteka | Fakultet za saobraćaj i komunikacije</title>
+        <meta
+          name="description"
+          content="Website biblioteke Fakulteta za saobraćaj i komunikacije Univerziteta u Sarajevu."
+        />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      <main>
+        <h1>Test</h1>
+      </main>
+    </div>
+  );
+}
