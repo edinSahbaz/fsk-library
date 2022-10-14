bdf7bb5c9f24eaf548126b5c2b9649858b8e820e
Faris Rizvanovic <faris.rizvanovic00@gmail.com>
2022-10-14 23:23:04 +0200

    Re-added searchbar and fixed it's colors

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,6 +1,9 @@
+import Searchbar from "../components/Searchbar";
+
 export default function Home() {
   return (
     <div>
+      <Searchbar />
       <div style={{ height: "100vh" }}></div>
     </div>
   );
