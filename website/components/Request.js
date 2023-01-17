9e84f26222701f5e44cdee40b5a9a4033edb0640
Faris Rizvanovic <faris.rizvanovic00@gmail.com>
2023-01-17 01:18:23 +0100

    Inital commit

--- website/components/Request.js
+++ website/components/Request.js
@@ -11,40 +11,54 @@ import {
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { db } from "../lib/firebase";
+import styles from "../styles/Request.module.css";
 
-const Request = ({ bookId, userId, id }) => {
+const Request = ({ bookId, userId, id, addedTime }) => {
   const [user, setUser] = useState("");
   const [bookName, setBookName] = useState("");
   const [bookQuantity, setBookQuantity] = useState(0);
+  const [bookISBN, setBookISBN] = useState("");
+  const [requestAddedDate, setRequestAddedDate] = useState();
 
   const getUserName = async () => {
-    const ref = doc(db, 'users', userId);
+    const ref = doc(db, "users", userId);
     const res = await getDoc(ref);
 
-    if(!res.exists()) return;
-    const {name, surname} = res.data();
-    if(name && surname) setUser(`${name} ${surname}`);
-  }
+    if (!res.exists()) return;
+    const { name, surname } = res.data();
+    if (name && surname) setUser(`${name} ${surname}`);
+  };
+
+  useEffect(() => {
+    var t = new Date(addedTime.seconds * 1000);
+    var date = t.toLocaleDateString("de-DE", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+    setRequestAddedDate(date);
+  }, []);
 
   useEffect(() => {
     getUserName();
-  }, [])
+  }, []);
 
   useEffect(() => {
-    if(!bookId) return;
+    if (!bookId) return;
 
-    const bookRef = doc(db, 'books', bookId);
-    const unsub = onSnapshot(bookRef, docSnap => {
-      if(!docSnap.exists()) return;
+    const bookRef = doc(db, "books", bookId);
+    const unsub = onSnapshot(bookRef, (docSnap) => {
+      if (!docSnap.exists()) return;
 
       const data = docSnap.data();
       setBookName(data.name);
       setBookQuantity(data.quantity);
-    })
+      setBookISBN(data.ISBN);
+    });
 
     return () => {
-      unsub()
-    }
+      unsub();
+    };
   }, []);
 
   const confirmLease = () => {
@@ -58,12 +72,12 @@ const Request = ({ bookId, userId, id }) => {
     }).then(() => {
       if (bookQuantity > 0) {
         updateDoc(doc(db, "books", bookId), {
-          quantity: bookQuantity - 1
+          quantity: bookQuantity - 1,
         });
       } else {
-        toast.error(`Knjiga ${bookName} nije dostupna!`)
+        toast.error(`Knjiga ${bookName} nije dostupna!`);
       }
-      
+
       deleteDoc(reqRef);
     });
 
@@ -75,12 +89,27 @@ const Request = ({ bookId, userId, id }) => {
   };
 
   return (
-    <div>
-      <label>
-        {user} ({userId}) - {bookName}
+    <tr>
+      <td className={`${styles.td}`}>{user}</td>
+      <td className={`${styles.td}`}>{bookName}</td>
+      <td className={`${styles.td}`}>{bookISBN}</td>
+      <td className={`${styles.td}`}>{bookQuantity}</td>
+      <td className={`${styles.td}`}>{requestAddedDate}.</td>
+      <td className={`${styles.td}`}>
+        <input type="date" />
+      </td>
+      <td className={`${styles.td} ${styles.options}`}>
+        <button className={styles.allow_button} onClick={confirmLease}>
+          Odobri
+        </button>
+        <button className={styles.deny_button}>Odbij</button>
+      </td>
+
+      {/* <label>
+        {"test " + user} ({userId}) - {bookName}
         <button onClick={confirmLease}>Odobri</button>
-      </label>
-    </div>
+      </label> */}
+    </tr>
   );
 };
 
--- website/components/admin/BookRequests.js
+++ website/components/admin/BookRequests.js
@@ -2,66 +2,92 @@ import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "../../lib/firebase";
 import Request from "../Request";
+import styles from "../../styles/BookRequests.module.css";
 
 const BookRequests = () => {
-    const [newestReq, setNewestReq] = useState(false);
-    const [requests, setRequest] = useState([]);
+  const [newestReq, setNewestReq] = useState(false);
+  const [requests, setRequest] = useState([]);
 
-    useEffect(() => {
-        const reqRef = collection(db, "bookRequests");
-        const reqQ = query(reqRef, orderBy("addedTime"));
+  useEffect(() => {
+    const reqRef = collection(db, "bookRequests");
+    const reqQ = query(reqRef, orderBy("addedTime"));
 
-        const unsubRequests = onSnapshot(reqQ, (colSnap) => {
-        const temp = [];
-        colSnap.forEach((req) => {
-            temp.push({ id: req.id, ...req.data() });
-        });
+    const unsubRequests = onSnapshot(reqQ, (colSnap) => {
+      const temp = [];
+      colSnap.forEach((req) => {
+        temp.push({ id: req.id, ...req.data() });
+      });
 
-        setRequest(temp);
-        });
+      setRequest(temp);
+    });
 
-        return () => {
-        unsubRequests();
-        };
-    }, []);
-
-    const changeSortReq = () => {
-        setNewestReq((prev) => !prev);
-        let temp = requests.reverse();
-
-        setRequest(temp);
+    return () => {
+      unsubRequests();
     };
+  }, []);
 
-    return ( 
-        <div>
-            <h2>Zahtjevi za knjige</h2>
-            <div>
-                <label>
-                Sortiraj
-                <button onClick={changeSortReq}>
-                    {newestReq ? "Najstariji prvo" : "Najnoviji prvo"}
-                </button>
-                </label>
-            </div>
-            <div>
-                {requests ? (
-                <>
-                    {requests.length > 0 ? (
-                    <>
-                        {requests.map((req) => (
+  const changeSortReq = () => {
+    setNewestReq((prev) => !prev);
+    let temp = requests.reverse();
+
+    setRequest(temp);
+  };
+
+  return (
+    <div>
+      <h2>Zahtjevi za knjige</h2>
+
+      <div>
+        {requests ? (
+          <>
+            {requests.length > 0 ? (
+              <>
+                {/*  */}
+                <div>
+                  <table className={styles.table} id="books">
+                    <thead>
+                      <tr>
+                        <th className={styles.th}>Ime i Prezime Studenta</th>
+                        <th className={`${styles.naziv_knjige} ${styles.th}`}>
+                          Naziv Knjige
+                        </th>
+                        <th className={styles.th}>ISBN</th>
+                        <th className={styles.th}>Trenutno Stanje</th>
+                        <th
+                          className={`${styles.sortable} ${styles.th}`}
+                          onClick={changeSortReq}
+                        >
+                          Datum Zahtjeva
+                          <span
+                            className={
+                              newestReq ? styles.arrow : styles.reversed_arrow
+                            }
+                          ></span>
+                        </th>
+                        <th className={styles.th}>Datum Vracanja</th>
+                        <th className={styles.th}>Opcije</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {requests.map((req) => (
                         <Request key={req.id} {...req} />
-                        ))}
-                    </>
-                    ) : (
-                    <p>Nema novih zahtjeva za knjige.</p>
-                    )}
-                </>
-                ) : (
-                <p>Nema novih zahtjeva za knjige.</p>
-                )}
-            </div>
-            </div>
-     );
-}
- 
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+
+                {/*  */}
+              </>
+            ) : (
+              <p>Nema novih zahtjeva za knjige.</p>
+            )}
+          </>
+        ) : (
+          <p>Nema novih zahtjeva za knjige.</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
 export default BookRequests;
