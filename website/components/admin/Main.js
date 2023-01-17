3e4fb63be3c4ce610ca8acc5c066ac4748aa5c5a
Faris Rizvanovic <faris.rizvanovic00@gmail.com>
2023-01-17 12:46:48 +0100

    Feature

--- website/components/admin/Main.js
+++ website/components/admin/Main.js
@@ -4,27 +4,61 @@ import { db } from "../../lib/firebase";
 import styles from "./../../styles/Admin.module.css";
 
 const Main = () => {
-  const [messages, setMessages] = useState([])
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    const colRef = collection(db, 'messages');
+    const colRef = collection(db, "messages");
 
-    const unsub = onSnapshot(colRef, colSnap => {
-      const temp = []
-      colSnap.forEach(msg => {
-        if(msg.exists()) temp.push(msg.data())
-      })
-      setMessages(temp)
+    const unsub = onSnapshot(colRef, (colSnap) => {
+      const temp = [];
+      colSnap.forEach((msg) => {
+        if (msg.exists()) temp.push(msg.data());
+      });
+      setMessages(temp);
     });
 
     return () => {
       unsub();
-    }
-  }, [])
+    };
+  }, []);
 
   return (
     <div className={styles.main}>
-      <div className={styles.heading}>
+      <table className={styles.table} id="books">
+        <thead>
+          <tr>
+            <th className={styles.th}>Ime i Prezime Studenta</th>
+            <th className={`${styles.naziv_knjige} ${styles.th}`}>Email</th>
+            <th className={styles.th}>ISBN</th>
+            <th className={styles.th}>Trenutno Stanje</th>
+            <th className={`${styles.sortable} ${styles.th}`}>Predmet</th>
+            <th className={styles.th}>Poruka</th>
+            <th className={styles.th}>Datum</th>
+          </tr>
+        </thead>
+        <tbody>
+          {messages.map((msg, i) => (
+            <tr>
+              <td className={`${styles.td}`}>{}</td>
+              <td className={`${styles.td}`}>{bookName}</td>
+              <td className={`${styles.td}`}>{bookISBN}</td>
+              <td className={`${styles.td}`}>{bookQuantity}</td>
+              <td className={`${styles.td}`}>{requestAddedDate}.</td>
+              <td className={`${styles.td}`}></td>
+              <td className={`${styles.td} ${styles.options}`}>
+                <button className={styles.allow_button} onClick={confirmLease}>
+                  Odobri
+                </button>
+                <button className={styles.deny_button} onClick={denyRequest}>
+                  Odbij
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {/* <div className={styles.heading}>
         <h2>Dashboard - Admin</h2>
         <div>
           {
@@ -35,7 +69,7 @@ const Main = () => {
             ))
           }
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
